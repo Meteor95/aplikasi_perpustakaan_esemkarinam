6b313f3972122b32505a5c38b19485b379499e1b
Meteor95 <mochamad.aries.styawan@gmail.com>
2024-07-22 19:43:01 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\ForceSSL;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->append(ForceSSL::class);
         $middleware->append(\Illuminate\Session\Middleware\StartSession::class);
         $middleware->validateCsrfTokens(except: [
             '/generate-csrf-token',
