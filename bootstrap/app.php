bf2305c618a0700cade4733785636e5c46aa2ece
Meteor95 <mochamad.aries.styawan@gmail.com>
2024-07-22 22:34:48 +0700

    test

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(ForceSSL::class);
         $middleware->append(\Illuminate\Session\Middleware\StartSession::class);
         $middleware->validateCsrfTokens(except: [
             '/generate-csrf-token',
