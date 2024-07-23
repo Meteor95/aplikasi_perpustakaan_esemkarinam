f5acb0016f42d1f10642597c846fc307deb1f9b4
Meteor95 <mochamad.aries.styawan@gmail.com>
2024-07-23 07:59:17 +0700

    test kamera belakang

--- public/v1/assets/js/app/tambah_buku_siswa_pinjaman.js
+++ public/v1/assets/js/app/tambah_buku_siswa_pinjaman.js
@@ -14,11 +14,11 @@ $(function() {
 });
 let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
 scanner.addListener('scan', function (content) {
-    prosestambahkeranjang(kodeitem)
+    prosestambahkeranjang(content)
 });
 Instascan.Camera.getCameras().then(function (cameras) {
   if (cameras.length > 0) {
-    scanner.start(cameras[0]);
+    scanner.start(cameras[1]);
   } else {
     console.error('No cameras found.');
   }
