afec57eaa8a9e125f68b3371562bfe13f64b32ca
Meteor95 <mochamad.aries.styawan@gmail.com>
2024-07-24 14:36:08 +0700

    hasil akhir peminjaman

---
+++ public/v1/assets/js/app/buku_pinjaman_siswa.js
@@ -0,0 +1,160 @@
+$(function() {
+    loaddatatables();
+});
+function loaddatatables(){
+    $.get('/generate-csrf-token', function(response) {
+         let daftar_peminjaman = $("#daftar_peminjaman").DataTable({
+            language:{
+                "paginate": {
+                    "first": '<i class="ri-arrow-go-back-line"></i>', 
+                    "last": '<i class="ri-arrow-go-forward-line"></i>', 
+                    "next": '<i class="ri-arrow-right-circle-line"></i>', 
+                    "previous": '<i class="ri-arrow-left-circle-line"></i>',
+                },
+            },
+            dom: '<"top"ip>rt<"clear">', 
+            scrollCollapse: true,
+            scrollX: true,
+            bFilter: false,
+            bInfo : true,
+            ordering: false,
+            bPaginate: true,
+            bProcessing: true, 
+            serverSide: true,
+            ajax: {
+                "url": baseurlapi + '/perpustakaan/daftar_peminjam',
+                "type": "GET",
+                "beforeSend": function (xhr) { xhr.setRequestHeader("Authorization", "Bearer " + localStorage.getItem('session_id_browser'));},
+                "data": function (d) {
+                    d._token = response.csrf_token;
+                    d.parameter_pencarian = $('#kotak_pencarian').val();
+                },
+                "dataSrc": function (json) {
+                    let detailData = json.data;
+                    let mergedData = detailData.map(item => {
+                        return {
+                            ...item,
+                            recordsFiltered: json.recordsFiltered,
+                        };
+                    });
+                    return mergedData;
+                }
+            },
+            infoCallback: function (settings) {
+                if (typeof settings.json !== "undefined"){
+                    const currentPage = Math.floor(settings._iDisplayStart / settings._iDisplayLength) + 1;
+                    const recordsFiltered = settings.json.recordsFiltered;
+                    const infoString = 'Halaman ke: ' + currentPage + ' Ditampilkan: ' + 10 + ' Jumlah Data: ' + recordsFiltered+ ' data';
+                    return infoString;
+                }
+            },
+            pagingType: "full_numbers",
+            columnDefs: [{
+                defaultContent: "-",
+                targets: "_all"
+            }],
+            columns: [
+                {
+                    title: "No",
+                    render: function(data, type, row, meta) {
+                        if (type === 'display') {
+                            return meta.row + 1 + meta.settings._iDisplayStart;
+                        }
+                        return data;
+                    }
+                },
+                {
+                    title: "Nota Peminjam",
+                    render: function(data, type, row, meta) {
+                        if (type === 'display') {
+                            return "<a href=\"javascript:void(0)\" id=\"detailinformasi"+row.id_user+"\" onclick=\"detailinformasi('"+row.nomor_transkasi+"')\">"+row.nomor_transkasi+"</a>";
+                        }
+                        return data;
+                    }
+                }, 
+                {
+                    title: "Dipinjam Oleh / Admin Oleh",
+                    render: function(data, type, row, meta) {
+                        if (type === 'display') {
+                            return "Member : "+row.nama_lengkap+"<br>Admin : "+row.nama_lengkap_pegawai;
+                        }
+                        return '';
+                    }
+                },
+                {
+                    title: "Waktu Peminjaman",
+                    render: function(data, type, row, meta) {
+                        if (type === 'display') {
+                            return 'Dipinjam Pada : '+formatDateddmmyyyhhiiss(row.tanggal_peminjaman)+'<br>Dikembalikan Pada : '+formatDateddmmyyyhhiiss(row.tanggal_pengembalian);
+                        }
+                        return '';
+                    }
+                }, 
+                {
+                    title: "Total Buku Dipinjam",
+                    render: function(data, type, row, meta) {
+                        if (type === 'display') {
+                            return row.totaljenisbuku_dipinjam+" Jenis Buku<br>"+row.totalbuku_dipinjam+" Total Buku";
+                        }
+                        return '';
+                    }
+                }
+            ],
+        });
+    });
+}
+function detailinformasi(nomornota){
+    $.get('/generate-csrf-token', function(response) {
+        $.ajax({
+            "url": baseurlapi + "/perpustakaan/detail_pinjaman",
+            "type": 'GET',
+            "beforeSend": function (xhr) { xhr.setRequestHeader("Authorization", "Bearer " + localStorage.getItem('session_id_browser'));},
+            "dataType": 'json',
+            "data": {
+                _token: response.csrf_token,
+                parameter_pencarian:nomornota,
+            },
+            "complete": function() {
+            },
+            "success": function(response) {
+                if (response.success == false) {
+                    return toastr.error(response.message, 'Pesan Kesalahan Code : ' + response.rc);
+                }
+                $('#judulinformasidetail').html("Informasi Detail Peminjaman "+response.data[0].nomor_transkasi)
+                $('#id_siswa').html(response.data[0].nis)
+                $('#nama_siswa').html(response.data[0].nama_lengkap)
+                $('#nomor_telp').html(response.data[0].nomor_kontak)
+                $('#kelas').html(response.data[0].nama_kelas)
+                $('#tahun_ajaran').html(response.data[0].keterangan_tahun_ajaran)
+                let totalbaris = 0;
+                let currentClasses = $('#tabeldetail').attr('class');
+                $('#tabeldetail').find('tbody').empty();
+                $.each(response.data, function(index, item) {
+                    totalbaris = totalbaris + 1;
+                    let row = '<tr>' +
+                                '<td>' + item.id_buku + '</td>' +
+                                '<td>' + item.nama_buku + '</td>' +
+                                '<td>' + item.qty_pinjam + ' Buku</td>' +
+                                '<td>' + item.approval_status + '</td>' +
+                                '<td>' +
+                            '</td>' +
+                             '</tr>';
+                    $('#tabeldetail').find('tbody').append(row);
+                });
+                $('#totaljenisbuku').html(totalbaris)
+                $('#tabeldetail').attr('class', currentClasses);
+                $('#tabeldetail').addClass('table table-bordered dt-responsive nowrap table-striped align-middle');
+                $('#informasi_detail_peminjaman').modal('toggle');
+            },
+            "error": function(xhr, status, error) {
+                toastr.error('Terjadi kesalahan proses PENYIMPANAN INFORMASI PEMINJAMAN. Silahkan hubungi TIM Terkiat. Pesan Kesalahan : ' + xhr.responseJSON.message, 'Pesan REST API Tambah Informasi Buku');
+            }
+        });
+    });
+}
+$('#kotak_pencarian').on('input', debounce(function (e) { 
+    $('#daftar_peminjaman').DataTable().ajax.reload();
+}, 500));
+$('#pencarian_data').on('click', function() {
+    $('#daftar_peminjaman').DataTable().ajax.reload();
+});
